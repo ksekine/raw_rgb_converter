5b751d326dc336a76643a439b097e21b99fbcbce
ksekine <kazuki@navier.co.jp>
2022-11-03 20:25:04 +0900

    Updated readme.

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# RAW / RGB converter  
+# Raw / RGB converter  
+
+## Set environment  
+
+```
+python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
+
+## Raw (RGGB) to RGB  
+
+```
+python raw2rgb.py --load_dir [LOAD_DIR] --save_dir [SAVE_DIR] --width [IMAGE_WIDTH] --height [IMAGE_HEIGHT] --ext [SEARCH_EXTENSION]
+```
+
+## RGB to Raw (RGGB)  
+
+**Sampling pixels are not enough.**  
+
+```
+python rgb2raw.py --load_dir [LOAD_DIR] --save_dir [SAVE_DIR] --ext [SEARCH_EXTENSION]
+```
--- raw2rgb.py
+++ raw2rgb.py
@@ -19,52 +19,7 @@ def pack_raw_image(raw_image):
     return out
 
 
-def demosaic(image, mode='linear'):
-    # assert isinstance(image, torch.Tensor)
-    image = image.clamp(0.0, 1.0) * 255
-
-    if image.dim() == 4:
-        num_images = image.dim()
-        batch_input = True
-    else:
-        num_images = 1
-        batch_input = False
-        image = image.unsqueeze(0)  # [1, 4, 32, 32]
-
-    # padding （Bayer2RGBの出力がエッジ付近でおかしくなるため）
-    image = F.pad(image, (4, 4, 4, 4), "reflect")
-
-    # Generate single channel input for opencv
-    im_sc = torch.zeros((num_images, image.shape[-2] * 2, image.shape[-1] * 2, 1))
-    im_sc[:, ::2, ::2, 0] = image[:, 0, :, :]
-    im_sc[:, ::2, 1::2, 0] = image[:, 1, :, :]
-    im_sc[:, 1::2, ::2, 0] = image[:, 2, :, :]
-    im_sc[:, 1::2, 1::2, 0] = image[:, 3, :, :]
-
-    im_sc = im_sc.numpy().astype(np.uint8)
-
-    out = []
-
-    def shift(image, shifts=[0, 0]):  # add
-        h, w = image.shape[:2]
-        M = np.float32([[1, 0, shifts[0]], [0, 1, shifts[1]]])
-        return cv2.warpAffine(image, M, (w, h))
-
-    for im in im_sc:
-        # cv.imwrite('frames/tmp.png', im)
-        if mode == 'linear':
-            im_dem_np = cv2.cvtColor(im, cv2.COLOR_BAYER_BG2RGB)
-            im_dem_np = shift(im_dem_np, [0, 0])  # add
-        elif mode == 'vng':
-            im_dem_np = cv2.cvtColor(im, cv2.COLOR_BAYER_BG2RGB_VNG)
-            im_dem_np = shift(im_dem_np, [0, 2])  # add
-        elif mode == 'ea':
-            im_dem_np = cv2.cvtColor(im, cv2.COLOR_BAYER_BG2RGB_EA)
-            im_dem_np = shift(im_dem_np, [0, 0])  # add
-        im_dem_np_ = im_dem_np[8:-8, 8:-8, :]  # unpadding
-
-
-def demosaic2(raw_image):
+def demosaic(raw_image):
     raw_image = pack_raw_image(raw_image)
     raw_image = np.clip(raw_image, 0.0, 1.0) * 255.0
 
@@ -76,10 +31,12 @@ def demosaic2(raw_image):
     raw_image_sc = raw_image_sc.astype(np.uint8)
 
     rgb_image = cv2.cvtColor(raw_image_sc, cv2.COLOR_BAYER_BG2BGR)
-    rgb_image = rgb_image.astype(np.float32)
-    rgb_image = rgb_image / np.max(rgb_image)
-    rgb_image = np.clip(rgb_image, 0.0, 1.0) * 255.0
-    rgb_image = rgb_image.astype(np.uint8)
+    
+    # Rescaale pixel value range
+    # rgb_image = rgb_image.astype(np.float32)
+    # rgb_image = rgb_image / np.max(rgb_image)
+    # rgb_image = np.clip(rgb_image, 0.0, 1.0) * 255.0
+    # rgb_image = rgb_image.astype(np.uint8)
 
     return rgb_image
 
@@ -97,8 +54,7 @@ if __name__ == '__main__':
     for i, file in enumerate(files):
         raw_data = np.fromfile(file, np.float32)
         raw_image = raw_data.reshape(args.width, args.height)
-        # out = demosaic(pack_raw_image(data))
-        rgb_image = demosaic2(raw_image)
+        rgb_image = demosaic(raw_image)
 
         if not os.path.exists(args.save_dir):
             os.makedirs(args.save_dir)

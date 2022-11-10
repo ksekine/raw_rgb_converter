3201820d933de451b84b6bea36202daf4af1bea2
ksekine <kazuki@navier.co.jp>
2022-11-10 11:31:00 +0900

    Added data_type parameter.

--- rgb2raw.py
+++ rgb2raw.py
@@ -35,6 +35,7 @@ if __name__ == '__main__':
     parser.add_argument('--load_dir', type=str, required=True, help='input dir that contains rgb images')
     parser.add_argument('--save_dir', type=str, required=True, help='output dir that saves y, u and v images')
     parser.add_argument('--ext', type=str, default='png', help='search extension')
+    parser.add_argument('--data_type', type=str, default='float32', choices=['float32', 'uint8', 'uint16'], help='output data type')
     args = parser.parse_args()
 
     files = sorted(glob.glob(os.path.join(args.load_dir, '*.' + args.ext)))
@@ -43,7 +44,13 @@ if __name__ == '__main__':
         rgb_image = np.array(rgb_image)
 
         raw_image = mosaic(rgb_image)
-        raw_image = raw_image.astype(np.float32) / 255.0
+        if args.data_type == 'float32':
+            raw_image = raw_image.astype(np.float32) / 255.0
+        elif args.data_type == 'uint8':
+            raw_image = raw_image.astype(np.uint8)
+        elif args.data_type == 'uint16':
+            raw_image = raw_image.astype(np.float32) / 255.0 * 65535.0
+            raw_image = raw_image.astype(np.uint16)
 
         if not os.path.exists(args.save_dir):
             os.makedirs(args.save_dir)
